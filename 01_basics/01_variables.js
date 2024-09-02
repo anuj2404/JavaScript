fb606cddf7e4b5848b84aecbbc1ced332c2bd814
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-02 07:07:34 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 101122
+let accountEmail = "anuj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Noida" // not a good way to declare variable
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "abc@yahoo.com"
+accountPassword = "457894"
+accountCity = "Lucknow"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
