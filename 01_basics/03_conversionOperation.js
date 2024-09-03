f47810ef6efe4f24675776004f3a2c6665e7eff0
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-03 05:15:42 +0000

    conversion operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,4 +26,24 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
+
+//********* Operations *********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " anuj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "3")
+
+console.log(+true);
+console.log(+"")
