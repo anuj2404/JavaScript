105a42702b893eeaca15bea1b7aa1ea655265b85
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-04 16:36:53 +0000

    String

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -29,5 +29,6 @@ const url = "https://amazon.com/hitesh%20choudhary"
 console.log(url.replace('%20', '-' ))
 
 console.log(url.includes('anuj'))
+console.log(url.includes('ankur'))
 
 console.log(gameName.split('-'))
