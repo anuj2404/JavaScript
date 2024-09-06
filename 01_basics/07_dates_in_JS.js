61768254f5a3c797f0f686cb00c6853b53f0686c
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-06 16:54:48 +0000

    Dates

---
+++ 01_basics/07_dates_in_JS.js
@@ -0,0 +1,31 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+// let myCreateDate = new Date("2023-01-24")
+let myCreateDate = new Date("01-24-2023")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log((newDate.getMonth) + 1);
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
