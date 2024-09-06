f0b3d2a63e423442bc845eadff36867fd1d91e56
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-06 16:11:31 +0000

    Nums

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,17 +1,23 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966
 
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds = 100000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// Maths
+// Maths Library comes with JS by default
+
+
 
 
