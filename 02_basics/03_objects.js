d35d7aebf550fb6d87eb87d320c08f70ec2f7c1f
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-08 16:15:14 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,3 +1,28 @@
 // singleton
 // agar literals ki tarah declare karte toh singleton nhi banega
 // constructor se banega toh singleton banega
+
+
+// Object literals
+
+const mySym = Symbol("key1")
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Anuj Gupta",
+    [mySym]: "mykey1",
+    age: 24,
+    location:"Jaipur",
+    email: "hiteshchoudhary@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]); //symbol ka use square bracket ke andar karenge
+
+
+
+
+
