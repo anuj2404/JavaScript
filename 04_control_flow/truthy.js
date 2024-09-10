4673fd268a1c87883e24f823d21f2d49a1582689
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-10 05:53:28 +0000

    Truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,25 @@
+// const userEmail = "anuj@gmail.com"
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// Falsy Values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy Values
+// "0", 'false', " ", [], {}, function() {}//Empty function
+
+// const userEmail = []
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+    
+}
