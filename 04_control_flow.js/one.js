2fea5d31582ef14dc22de52d301643aa61bb3a30
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-10 04:55:38 +0000

    Control flow

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,11 @@
+// if statement
+const isUserloggedIn = true
+if (isUserloggedIn) {
+    // agar condition true hogi tb hi code execute hoga
+}
+
+if (2 == "2"){
+    console.log("executed");
+    
+}
+// <, >, <=, >=, ==, !=, ===, !==
