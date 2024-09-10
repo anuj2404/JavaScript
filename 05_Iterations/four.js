554ffaaa813e506a2127061214157f1fda3a20e6
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-10 15:28:37 +0000

    Iterations

---
+++ 05_Iterations/four.js
@@ -0,0 +1,30 @@
+// for in loop
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('UK', "United Kingdom")
+// map.set('Fr', "France")
+
+
+// for (const key in map){
+//     console.log(key);
+    
+// }
