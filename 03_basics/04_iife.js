457ec36562fb386c15ac6aee374536db3246baf2
Anuj Gupta <124883339+anuj2404@users.noreply.github.com>
2024-09-20 10:50:53 +0000

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -13,3 +13,15 @@
 })('anuj')
 
 // An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
+
+// (function () {
+//     // …
+//   })();
+  
+//   (() => {
+//     // …
+//   })();
+  
+//   (async () => {
+//     // …
+//   })();
